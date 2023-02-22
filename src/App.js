ea404e2dd266a5f0122d0abf37497c2c65905957
mariamhus <mariamhus2000@gmail.com>
2023-02-22 15:08:36 +0200

    Todo List using React

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TodoForm from './components/todoForm';
+import Todolist from './components/todolist';
+function App() {
+  return (
+    <div className="todo-app">
+     <Todolist/>
+    </div>
+  );
+}
+
+export default App;
